6e72540eb0d00217861d878f788f23722c5c2bf1
olenakutsir <olena.kutsir@gmail.com>
2016-05-04 19:42:12 +0000

    scr

--- js/script.js
+++ js/script.js
@@ -26,15 +26,10 @@ $(function() {
 })
 
 
+function scrollToId(whereToScroll) {
+    $('html, body').animate({
+        scrollTop: $(whereToScroll).offset().top
+    }, 800);
+    return true;
+}
 
-
-$(function () {
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-})
