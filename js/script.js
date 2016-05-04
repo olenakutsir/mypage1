0442d1e33699df68e39336fd41a3e0971c0b3980
olenakutsir <olena.kutsir@gmail.com>
2016-05-04 19:47:05 +0000

    boot

--- js/script.js
+++ js/script.js
@@ -33,3 +33,13 @@ function scrollToId(whereToScroll) {
     return true;
 }
 
+$(function () {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+})
