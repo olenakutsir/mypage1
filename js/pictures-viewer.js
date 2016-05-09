11d5b4ba68ba0f3fa6e16d5fc511b8a9f42936c8
olenakutsir <olena.kutsir@gmail.com>
2016-05-09 13:36:53 +0000

    d

--- js/pictures-viewer.js
+++ js/pictures-viewer.js
@@ -1,6 +1,23 @@
-$(function() {
-	$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
+$(document).ready(function() {
+
+	/* This is basic - uses default settings */
+	
+	$("a#photo").fancybox();
+	
+	/* Using custom settings */
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
 	});
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
 });
